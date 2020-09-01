478f3e8b576f8630ce45e85cab5d571ff97913f7
Jeremiah Abah <37028201+Jerryabah@users.noreply.github.com>
2020-09-01 09:59:26 -0700

    Initial Commit

---
+++ BasicClassesExercise/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BasicClassesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.Make = "Lexus";
+            car.Model = "RX350";
+            car.Year = 2021;
+
+            Console.WriteLine($"The Make of my next car is a {car.Make} SUV.");
+            Console.WriteLine($"The {car.Make} will be of the {car.Model} Model,");
+            Console.WriteLine($"and the Year of the SUV will be {car.Year}.");
+        }
+    }
+}
